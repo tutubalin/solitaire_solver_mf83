41dfff5b4c9d964307f2686bd2b094dba77b44f6
Daniil Tutubalin <tutubalin@gmail.com>
2024-03-26 18:21:09 +0700

    Added colors and flushing to profiler

--- profiler.py
+++ profiler.py
@@ -4,10 +4,10 @@ import sys
 
 @contextmanager
 def profile(comment):    
-    print(comment, end = '', file=sys.stderr)
+    print(comment, end = '', file=sys.stderr, flush=True)
     start_time = time.time_ns()
     try:
         yield
     finally:
         time_in_ms = (time.time_ns() - start_time)/1000000
-        print(f' Done in {time_in_ms}ms', file=sys.stderr)
+        print(f'\033[1;32m Done\033[0m in \033[1;33m{time_in_ms:.2f}\033[0m ms', file=sys.stderr, flush=True)
