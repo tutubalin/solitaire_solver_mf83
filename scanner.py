da302a4f15aaed2846292a68728fb9e04d00c55b
Daniil Tutubalin <tutubalin@gmail.com>
2024-03-26 18:21:47 +0700

    Scanner now works indefinitely polling for screenshot

--- scanner.py
+++ scanner.py
@@ -1,5 +1,9 @@
 from profiler import profile
 
+with profile('Importing built-ins...'):
+    import sys
+    import os
+    import time
 with profile('Importing PIL...'):
     from PIL import Image
 with profile('Importing numpy...'):    
@@ -8,8 +12,6 @@ with profile('Importing torch...'):
     import torch
 with profile('Importing model...'):        
     from model import CardDetector
-with profile('Importing sys...'):        
-    import sys
 
 NUM_WIDTH = 72
 NUM_X_OFFSET = 371
@@ -18,32 +20,45 @@ NUM_Y_OFFSET = 72
 START_X = 1080
 START_Y = 74
 
-filename = sys.argv[1]
+filename = "tmp/screenshot.png"
 # device = "cuda"
 device = "cpu"
 
 with profile('Loading model...'):
     model = torch.load('model/model.cpkt').to(device)
 
-with profile(f'Opening {filename}...'):
-    screenshot = Image.open(filename).convert('F')
+print('\033[1;32mReady\033[0m', file=sys.stderr, flush=True)
 
-with profile('Getting images...'):
-    card_images = list()
-    for i in range(4):
-        x = NUM_X_OFFSET*i + START_X
-        for j in range(13):
-            y = NUM_Y_OFFSET*j + START_Y
-            num = screenshot.crop((x,y,x+NUM_WIDTH,y+NUM_HEIGHT)).resize((16,16))
-            num_arr = np.array(num)/255
+while True:
 
-            card_images.append([num_arr])
+    if os.path.isfile(filename):
+        time.sleep(0.1)
+        print('\033[1;36mScreenshot found\033[0m', file=sys.stderr, flush=True)
 
-    input = torch.tensor(np.array(card_images)).to(device)
+        with profile(f'Opening screenshot...'):
+            screenshot = Image.open(filename).convert('F')
+            os.remove(filename)
 
-with profile('Scanning...'), torch.no_grad():
-    model.eval()
-    layout = (torch.argmax(model(input), dim=1)+torch.tensor(1)).cpu().view((4,13))
+        with profile('Getting images...'):
+            card_images = list()
+            for i in range(4):
+                x = NUM_X_OFFSET*i + START_X
+                for j in range(13):
+                    y = NUM_Y_OFFSET*j + START_Y
+                    num = screenshot.crop((x,y,x+NUM_WIDTH,y+NUM_HEIGHT)).resize((16,16))
+                    num_arr = np.array(num)/255
 
-for column in layout:
-    print(" ".join(str(int(x)) for x in column))
+                    card_images.append([num_arr])
+
+            input = torch.tensor(np.array(card_images)).to(device)
+
+        with profile('Scanning...'), torch.no_grad():
+            model.eval()
+            layout = (torch.argmax(model(input), dim=1)+torch.tensor(1)).cpu().view((4,13))
+
+        for column in layout:
+            print(" ".join(str(int(x)) for x in column))
+        
+        print('\033[1;32mReady\033[0m', file=sys.stderr, flush=True)
+    else:
+        time.sleep(0.5)
